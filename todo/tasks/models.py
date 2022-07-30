14766dafdda20f3471f6f41a8e060a40e8d9f16e
Rafael <rafaelmoura7033@gmail.com>
2022-07-30 15:41:18 -0300

    Criando model de Task

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Task(models.Model):
+    title = models.CharField(max_length=255)
+    description = models.TextField()
+    done = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.title

ece7b68216134e6e7d0d00dfd377c66c99951d87
Rafael <rafaelmoura7033@gmail.com>
2022-07-30 17:50:37 -0300

    Criando func de buscar todas as tasks

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,6 +1,13 @@
-# from django.shortcuts import render
+from django.shortcuts import render
+from .models import Task
 from django.http import HttpResponse
 
 
 def helloWorld(request):
     return HttpResponse("Hello World!")
+
+
+def get_tasks(request):
+    tasks = Task.objects.all()
+    print(tasks)
+    return render(request, 'tasks/tasks.html', {'tasks': tasks})

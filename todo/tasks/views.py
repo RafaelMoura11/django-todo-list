8e3044f1ddf86ca14e5fd6f0e4e1338041b1edcb
Rafael <rafaelmoura7033@gmail.com>
2022-07-31 12:33:13 -0300

    Criando func edit_task

--- todo/tasks/views.py
+++ todo/tasks/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from .models import Task
 from django.http import HttpResponse
 from .forms import TaskForm
@@ -16,8 +16,20 @@ def get_tasks(request):
 
 def task_details(request, id):
     task = get_object_or_404(Task, pk=id)
-    form = TaskForm(task)
-    return render(request, 'tasks/task-details.html', {
-        'task': task,
-        'form': form
-        })
+    return render(request, 'tasks/task-details.html', {'task': task})
+
+
+def edit_task(request, id):
+    task = get_object_or_404(Task, pk=id)
+    form = TaskForm(instance=task)
+    if request.method == 'POST':
+        form = TaskForm(request.POST, instance=task)
+        if form.is_valid():
+            task.save()
+            return redirect('/')
+        else:
+            return render(request, 'tasks/edit-task.html', {
+                'form': form, 'task': task})
+    else:
+        return render(request, 'tasks/edit-task.html', {
+            'form': form, 'task': task})

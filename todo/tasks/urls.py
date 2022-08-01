6ca70b804b54c91db72123b7bf3a43e9b5642495
Rafael <rafaelmoura7033@gmail.com>
2022-08-01 10:50:40 -0300

    Adicionando create-task em urls

--- todo/tasks/urls.py
+++ todo/tasks/urls.py
@@ -5,5 +5,6 @@ from . import views
 urlpatterns = [
   path('', views.get_tasks, name='tasks'),
   path('details/<int:id>', views.task_details, name="task-details"),
-  path('edit/<int:id>', views.edit_task, name="edit-task")
+  path('edit/<int:id>', views.edit_task, name="edit-task"),
+  path('create', views.create_task, name="create-task")
 ]

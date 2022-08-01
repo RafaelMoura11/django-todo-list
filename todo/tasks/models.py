347d721de1d4bda3d627e22fedd1f4657a676cb2
Rafael <rafaelmoura7033@gmail.com>
2022-08-01 17:09:01 -0300

    Adicionando user em models de task

--- todo/tasks/models.py
+++ todo/tasks/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from django.contrib.auth import get_user_model
 
 
 class Task(models.Model):
@@ -7,6 +8,8 @@ class Task(models.Model):
     done = models.BooleanField(default=False)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self):
         return self.title
